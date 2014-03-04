b2a02bc00d66f6a7a77176fd4e5c6c1b2fab3eda
Levon <lemarwin42@gmail.com>
2014-03-04 23:32:55 +0400

    Fittnes now calculated via Indi.  Problem with mutation.

--- src/main.d
+++ src/main.d
@@ -16,33 +16,15 @@ static double[] getVariables()
 	return globalVars;
 }
 
-double fittness(double[][] data, Expr func)
-{
-	int m = data.length;
-	int n = data[0].length;
-	double mse=0;
-	double ans=0;
-	for(int i=0;i<m;i++)
-	{
-		globalVars = data[i][0..$-1];
-		ans = data[i][$-1];
-		double delta = func.eval-ans;
-		mse+=delta*delta;
-		writeln(getVariables(),"\t",func.eval);
-	}
-	mse=mse/m;
-	return mse;
-}
-
 int main()
 {
 	double[][] data = getData("testdata.csv");
 	globalVars = data[0][0..$-1];
-	Expr c = getRandExpr(4);
+	Expr c = getRandExpr(2);
 	writeln(c.print);
 	writeln(c.offsprings);
-	writeln(fittness(data, c));
 	Indi jones = new Indi(c);
 	writeln(jones.fittness(data));
+	writeln(jones.pickRand().print);
 	return 0;
 }
